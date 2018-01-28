31b8ff57ac844c7ef368dc435453176300d5ca85
Huy Vo <huyvanvo94@gmail.com>
2018-01-28 11:25:18 -0800

    add Procfile file

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const multipartMiddleware = multipart();
 // Create the app.
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 
 app.use(cookieSession({
     secret: 'post-it',
